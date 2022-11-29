a643d916aca5ad8161dacf8efb6dd1864f6b1317
KM <31863107+kodmkr@users.noreply.github.com>
2022-11-29 18:27:58 +0100

    Init

---
+++ aoc22.asd
@@ -0,0 +1,41 @@
+;;;; aoc22.asd
+
+(asdf:defsystem #:aoc22
+  :description "???"
+  :depends-on ("cl-ppcre"
+               "arrow-macros"
+               "alexandria"
+               "anaphora"
+               "fiveam"
+               "bit-smasher")
+  :version "0.0.1"
+  :serial t
+  :components ((:file "package")
+               (:file "day01")
+               (:file "day02")
+               (:file "day03")
+               (:file "day04")
+               (:file "day05")
+               (:file "day06")
+               (:file "day07")
+               (:file "day08")
+               (:file "day09")
+               (:file "day10")
+               (:file "day11")
+               (:file "day12")
+               (:file "day13")
+               (:file "day14")
+               (:file "day15")
+               (:file "day16")
+               (:file "day17")
+               (:file "day18")
+               (:file "day19")
+               (:file "day20")
+               (:file "day21")
+               (:file "day22")
+               (:file "day23")
+               (:file "day24")
+               (:file "day25")
+               (:file "aoc22")))
+
+
---
+++ package.lisp
@@ -0,0 +1,81 @@
+;;;; package.lisp
+
+(defpackage #:aoc22
+  (:use #:cl))
+
+(defpackage :day01
+  (:use :cl :arrow-macros))
+
+(defpackage :day02
+  (:use :cl :arrow-macros))
+
+(defpackage :day03
+  (:use :cl :arrow-macros))
+
+(defpackage :day04
+  (:use :cl :arrow-macros))
+
+(defpackage :day05
+  (:use :cl :arrow-macros))
+
+(defpackage :day06
+  (:use :cl :arrow-macros))
+
+(defpackage :day07
+  (:use :cl :arrow-macros))
+
+(defpackage :day08
+  (:use :cl :arrow-macros))
+
+(defpackage :day09
+  (:use :cl :arrow-macros))
+
+(defpackage :day10
+  (:use :cl :arrow-macros))
+
+(defpackage :day11
+  (:use :cl :arrow-macros))
+
+(defpackage :day12
+  (:use :cl :arrow-macros))
+
+(defpackage :day13
+  (:use :cl :arrow-macros))
+
+(defpackage :day14
+  (:use :cl :arrow-macros))
+
+(defpackage :day15
+  (:use :cl :arrow-macros)
+  (:local-nicknames (:ax :alexandria)))
+
+(defpackage :day16
+  (:use :cl :arrow-macros))
+
+(defpackage :day17
+  (:use :cl :arrow-macros))
+
+(defpackage :day18
+  (:use :cl :arrow-macros))
+
+(defpackage :day19
+  (:use :cl :arrow-macros))
+
+(defpackage :day20
+  (:use :cl :arrow-macros))
+
+(defpackage :day21
+  (:use :cl :arrow-macros))
+
+(defpackage :day22
+  (:use :cl :arrow-macros))
+
+(defpackage :day23
+  (:use :cl :arrow-macros))
+
+(defpackage :day24
+  (:use :cl :arrow-macros))
+
+(defpackage :day25
+  (:use :cl :arrow-macros))
+
