f81bfd4325989948552d7929761569b6aa6f4962
KM <31863107+kodmkr@users.noreply.github.com>
2022-12-06 19:39:27 +0100

    Day06: done

--- day06.lisp
+++ day06.lisp
@@ -1,1 +1,24 @@
 (in-package :day06)
+
+(defun read-input (&optional (input-path "./example/day0601"))
+  (let ((s (alexandria:read-file-into-string input-path)))
+    s))
+
+(defun process (signal &key (view-width 4))
+  (let ((len (length signal)))
+    (loop for i from 0 below (- len view-width)
+          for view = (make-array view-width :displaced-to signal
+                                            :displaced-index-offset i)
+          for uniq = (remove-duplicates view :test #'char-equal)
+          if (= view-width (length uniq)) do
+            (return-from process (+ i view-width)))))
+
+;; 1538
+(defun sol1 (&optional (input-path "./input/day06"))
+  (-<> (read-input input-path)
+    (process <>)))
+
+;; 2315
+(defun sol2 (&optional (input-path "./input/day06"))
+  (-<> (read-input input-path)
+    (process <> :view-width 14)))
